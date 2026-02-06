b2b434eef2acea6d1bd20ff651873b34bc1f56a2
Kandarp Sharma <kndrpsharma@gmail.com>
2026-02-06 20:51:11 +0530

    Add Kafka listener for incoming transactions

---
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -0,0 +1,20 @@
+package com.jpmc.midascore.kafka;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class TransactionListener {
+
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+              groupId = "midas-core-group"
+    )
+    public void receive(Transaction transaction)
+    {
+
+        int amount = (int) transaction.getAmount();
+    }
+}
